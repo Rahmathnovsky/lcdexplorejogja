8ba6119e0b91d69061cf4442c77d236d3ec0ea92
zaychsual <zaychsual@gmail.com>
2023-11-09 14:06:39 +0700

    add email

--- send_email.php
+++ send_email.php
@@ -1,24 +1,36 @@
 <?php
+require 'vendor/autoload.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Ambil data dari formulir
     $name = $_POST["name"];
     $email = $_POST["email"];
     $subject = $_POST["subject"];
-    $message = $_POST["message"];
-    
-    $to = "aziz.zaind@yahoo.com"; // Replace with the actual email address
-    $subject = "Contact Form Submission: $subject";
-    $headers = "From: cyber1duck@gmail.com\r\n" .
-           "Reply-To: cyber1duck@gmail.com\r\n" .
-           "X-Mailer: PHP/" . phpversion();
-    
-    $mailBody = "Name: $name\nEmail: $email\nSubject: $subject\nMessage:\n$message";
-    
-    if (mail($to, $subject, $mailBody, $headers)) {
-        echo "Email sent successfully!";
+    $messageText = $_POST["message"];
+
+    // Buat objek Transporter dengan SMTP Gmail
+    $transport = (new Swift_SmtpTransport('smtp.gmail.com', 587, 'tls'))
+      ->setUsername('others.cookies@gmail.com') // Ganti dengan alamat email Anda
+      ->setPassword('@Password.1'); // Ganti dengan kata sandi aplikasi Gmail yang dihasilkan
+
+    // Buat objek Mailer menggunakan Transporter
+    $mailer = new Swift_Mailer($transport);
+
+    // Buat objek Pesan
+    $message = (new Swift_Message($subject))
+      ->setFrom([$email => $name])
+      ->setTo(['recipient@example.com' => 'Recipient Name']) // Ganti dengan alamat email penerima
+      ->setBody($messageText);
+
+    // Kirim email
+    $result = $mailer->send($message);
+
+    if ($result) {
+        echo "Email sent successfully.";
     } else {
-        echo "Email sending failed. Please try again later.";
+        echo "Email sending failed.";
     }
 } else {
-    echo "Invalid request";
+    // Jika bukan metode POST, tampilkan pesan kesalahan
+    echo "Invalid request.";
 }
-?>
